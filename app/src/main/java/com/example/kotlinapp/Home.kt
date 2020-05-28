b4e2e4d70c32d105f6e07c89e015d3938a5b0822
Yohanathan-Kugavathanan <kugajpx2012@gmail.com>
2020-05-28 18:40:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/kotlinapp/Home.kt
@@ -0,0 +1,17 @@
+package com.example.kotlinapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Home : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+
+
+
+
+
+}
